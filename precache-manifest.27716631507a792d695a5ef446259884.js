233f30080e39d14527a8c2bca70dcc7d20f7311d
nishanth r <nishanth.ramesh1@gmail.com>
2019-08-31 19:26:12 +0530

    Updates

---
+++ precache-manifest.27716631507a792d695a5ef446259884.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f26fafbb490baa76c1cfdda15ef3fd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e671f46b5a19cb75d55",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "32a5fbd386b4e7d1b0df",
+    "url": "/static/js/2.fc2448ba.chunk.js"
+  },
+  {
+    "revision": "8e671f46b5a19cb75d55",
+    "url": "/static/js/main.4730065e.chunk.js"
+  },
+  {
+    "revision": "bf4bc32e454fc9102f16",
+    "url": "/static/js/runtime~main.cc8fd42b.js"
+  }
+]);
